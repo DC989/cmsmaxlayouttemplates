21c557c9f677951aa6538ba400688ffcf51b4047
Ivana <ivanakarakacev28@gmail.com>
2022-11-30 16:04:25 +0100

    buttons-regular-1 version created.

---
+++ button-samples.blade.php
@@ -0,0 +1,134 @@
+@extends('layouts.master')
+
+@section('head-title')
+    Button Samples
+    @parent
+@stop
+
+@section('page-title')
+    <h1>Button Samples</h1>
+@stop
+
+@section('content')
+    <div style="background:bisque" class="text-center">
+        <h3>Primary</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-primary btn-sm">PRIMARY SM</a></p>
+        <p><a href="#" class="btn btn-primary">PRIMARY RG</a></p>
+        <p><a href="#" class="btn btn-primary btn-lg">PRIMARY LG</a></p>
+        <p><a href="#" class="btn btn-primary btn-xl">PRIMARY XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>White Border</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-primary btn-sm btn-white-border btn-white-border--sm">WHITE BORDER SM</a></p>
+        <p><a href="#" class="btn btn-primary btn-white-border">WHITE BORDER RG</a></p>
+        <p><a href="#" class="btn btn-primary btn-lg btn-white-border btn-white-border--lg">WHITE BORDER LG</a></p>
+        <p><a href="#" class="btn btn-primary btn-xl btn-white-border btn-white-border--xl">WHITE BORDER XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Dark Border</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-primary btn-sm btn-dark-border btn-dark-border--sm">DARK BORDER SM</a></p>
+        <p><a href="#" class="btn btn-primary btn-dark-border">DARK BORDER RG</a></p>
+        <p><a href="#" class="btn btn-primary btn-lg btn-dark-border btn-dark-border--lg">DARK BORDER LG</a></p>
+        <p><a href="#" class="btn btn-primary btn-xl btn-dark-border btn-dark-border--xl">DARK BORDER XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Primary Border</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-primary btn-sm btn-primary-border btn-primary-border--sm">PRIMARY SM</a></p>
+        <p><a href="#" class="btn btn-primary btn-primary-border">PRIMARY RG</a></p>
+        <p><a href="#" class="btn btn-primary btn-lg btn-primary-border btn-primary-border--lg">PRIMARY LG</a></p>
+        <p><a href="#" class="btn btn-primary btn-xl btn-primary-border btn-primary-border--xl">PRIMARY XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Secondary</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-secondary btn-sm">SECONDARY SM</a></p>
+        <p><a href="#" class="btn btn-secondary">SECONDARY RG</a></p>
+        <p><a href="#" class="btn btn-secondary btn-lg">SECONDARY LG</a></p>
+        <p><a href="#" class="btn btn-secondary btn-xl">SECONDARY XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Success</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-success btn-sm">SUCCESS SM</a></p>
+        <p><a href="#" class="btn btn-success">SUCCESS RG</a></p>
+        <p><a href="#" class="btn btn-success btn-lg">SUCCESS LG</a></p>
+        <p><a href="#" class="btn btn-success btn-xl">SUCCESS XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Danger</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-danger btn-sm">DANGER SM</a></p>
+        <p><a href="#" class="btn btn-danger">DANGER RG</a></p>
+        <p><a href="#" class="btn btn-danger btn-lg">DANGER LG</a></p>
+        <p><a href="#" class="btn btn-danger btn-xl">DANGER XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Warning</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-warning btn-sm">WARNING SM</a></p>
+        <p><a href="#" class="btn btn-warning">WARNING RG</a></p>
+        <p><a href="#" class="btn btn-warning btn-lg">WARNING LG</a></p>
+        <p><a href="#" class="btn btn-warning btn-xl">WARNING XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Info</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-info btn-sm">INFO SM</a></p>
+        <p><a href="#" class="btn btn-info">INFO RG</a></p>
+        <p><a href="#" class="btn btn-info btn-lg">INFO LG</a></p>
+        <p><a href="#" class="btn btn-info btn-xl">INFO XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Light</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-light btn-sm">LIGHT SM</a></p>
+        <p><a href="#" class="btn btn-light">LIGHT RG</a></p>
+        <p><a href="#" class="btn btn-light btn-lg">LIGHT LG</a></p>
+        <p><a href="#" class="btn btn-light btn-xl">LIGHT XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Dark</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-dark btn-sm">DARK SM</a></p>
+        <p><a href="#" class="btn btn-dark">DARK RG</a></p>
+        <p><a href="#" class="btn btn-dark btn-lg">DARK LG</a></p>
+        <p><a href="#" class="btn btn-dark btn-xl">DARK XL</a></p>
+
+        <div>&nbsp;</div>
+
+        <h3>Link</h3>
+        <div>&nbsp;</div>
+
+        <p><a href="#" class="btn btn-link btn-sm">LINK SM</a></p>
+        <p><a href="#" class="btn btn-link">LINK RG</a></p>
+        <p><a href="#" class="btn btn-link btn-lg">LINK LG</a></p>
+        <p><a href="#" class="btn btn-link btn-xl">LINK XL</a></p>
+
+        <div>&nbsp;</div>
+    </div>
+@stop
